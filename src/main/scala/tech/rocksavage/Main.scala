6131561841bc1b29543116968d27b257ef7eece1
Nick Allison <nickrallison1@gmail.com>
2025-01-11 15:56:26 -0700

    Added scaladocs to Main.scala

--- src/main/scala/tech/rocksavage/Main.scala
+++ src/main/scala/tech/rocksavage/Main.scala
@@ -7,16 +7,26 @@ import tech.rocksavage.traits.ModuleConfig
 import tech.rocksavage.synth.Synth.{genVerilogFromModuleName, synthesizeFromModuleName}
 import scala.sys.exit
 
+/**
+ * The main entry point for the application.
+ * This object handles command-line arguments and delegates tasks to either the VerilogGenerator or Synthesizer.
+ */
 object Main {
+
+  /**
+   * The main method that processes command-line arguments and executes the appropriate subcommand.
+   *
+   * @param args_array Command-line arguments passed to the application.
+   */
   def main(args_array: Array[String]): Unit = {
-    val conf = new Conf(args_array.toIndexedSeq)
-    val defaultConfigs = ConfigLoader.loadConfigs(conf)
-    val build_folder = new File("out")
+    val conf = new Conf(args_array.toIndexedSeq) // Parse command-line arguments
+    val defaultConfigs = ConfigLoader.loadConfigs(conf) // Load configurations based on the subcommand
+    val build_folder = new File("out") // Define the output directory
 
     conf.subcommand match {
-      case Some(conf.verilog) => VerilogGenerator.generate(conf, defaultConfigs)
-      case Some(conf.synth)   => Synthesizer.synthesize(conf, defaultConfigs, build_folder)
-      case _                  => println("No subcommand given")
+      case Some(conf.verilog) => VerilogGenerator.generate(conf, defaultConfigs) // Generate Verilog
+      case Some(conf.synth)   => Synthesizer.synthesize(conf, defaultConfigs, build_folder) // Synthesize the design
+      case _                  => println("No subcommand given") // Handle invalid or missing subcommand
     }
   }
 }
