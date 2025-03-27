feae65c60b98d5ec61d050711538f78ae9ff3969
Nick Allison <nickrallison1@gmail.com>
2025-03-27 16:16:50 -0600

    progress

--- src/main/scala/tech/rocksavage/Docs.scala
+++ src/main/scala/tech/rocksavage/Docs.scala
@@ -3,15 +3,12 @@ package tech.rocksavage
 import tech.rocksavage.args.Conf
 
 import java.io.File
+import scala.sys.exit
 
-/** An object responsible for synthesizing the design based on the provided
-  * configurations.
-  */
+/** */
 object Docs {
 
-    /** Synthesizes the design for the specified module and configurations.
-      *
-      * @param conf
+    /** @param conf
       *   The parsed command-line arguments.
       * @param defaultConfigs
       *   A map of configuration names to their corresponding parameters.
@@ -23,8 +20,50 @@ object Docs {
         defaultConfigs: Map[String, Any],
         build_folder: File
     ): Unit = {
-        Synthesizer.synthesize(conf, defaultConfigs, build_folder)
-        Sta.sta(conf, defaultConfigs, build_folder)
+//        Synthesizer.synthesize(conf, defaultConfigs, build_folder)
+//        Sta.sta(conf, defaultConfigs, build_folder)
+
+        val synth_files: Map[String, File] =
+            new File(s"$build_folder/synth/").listFiles
+                .filter(_.toString.endsWith("gates.txt"))
+                .map(file => file.toString.split("/").last -> file)
+                .toMap
+
+        println(synth_files)
+        exit(0)
+
+        val gate_count_map: Map[String, String] = synth_files.map { entry =>
+            val file  = entry._2
+            val name  = entry._1
+            val gates = scala.io.Source.fromFile(file).toString()
+            name -> gates
+        }.toMap
+
+        val sta_files: Map[String, File] =
+            new File(s"$build_folder/sta/").listFiles
+                .filter(_.toString.endsWith("slack.txt"))
+                .map(file => file.toString.split("/").last -> file)
+                .toMap
+
+        val wns_map: Map[String, String] = sta_files.map { entry =>
+            val file = entry._2
+            val name = entry._1
+            val wns  = scala.io.Source.fromFile(file).toString()
+            name -> wns
+        }.toMap
+
+        val joined_map: Map[String, (String, String)] = gate_count_map.map {
+            case (name, gates) =>
+                name -> (gates, wns_map(name))
+        }
+        for ((name, (gates, wns)) <- joined_map) {
+            val content = s"""|Module: $name
+                              |Gates: $gates
+                              |WNS: $wns
+                              |""".stripMargin
+            writeFile(s"$build_folder/docs/$name.txt", content)
+        }
+
     }
 
     /** Writes the given content to a file at the specified path.
