89c3c70d0532d41388270337d4c0ea60cc4d2d10
Nick Allison <nickrallison1@gmail.com>
2025-03-27 17:15:46 -0600

    fixing workflows

--- src/main/scala/tech/rocksavage/Docs.scala
+++ src/main/scala/tech/rocksavage/Docs.scala
@@ -3,7 +3,6 @@ package tech.rocksavage
 import tech.rocksavage.args.Conf
 
 import java.io.File
-import scala.sys.exit
 
 /** */
 object Docs {
@@ -20,35 +19,32 @@ object Docs {
         defaultConfigs: Map[String, Any],
         build_folder: File
     ): Unit = {
-//        Synthesizer.synthesize(conf, defaultConfigs, build_folder)
-//        Sta.sta(conf, defaultConfigs, build_folder)
+        Synthesizer.synthesize(conf, defaultConfigs, build_folder)
+        Sta.sta(conf, defaultConfigs, build_folder)
 
-        val synth_files: Map[String, File] =
+        val synth_files: Map[String, String] =
             new File(s"$build_folder/synth/").listFiles
-                .filter(_.toString.endsWith("gates.txt"))
-                .map(file => file.toString.split("/").last -> file)
+                .filter(file => (new File(file + "/gates.txt").exists()))
+                .map(file => file.getName -> (file + "/gates.txt"))
                 .toMap
-
-        println(synth_files)
-        exit(0)
 
         val gate_count_map: Map[String, String] = synth_files.map { entry =>
             val file  = entry._2
             val name  = entry._1
-            val gates = scala.io.Source.fromFile(file).toString()
+            val gates = scala.io.Source.fromFile(file).getLines().mkString
             name -> gates
         }.toMap
 
-        val sta_files: Map[String, File] =
+        val sta_files: Map[String, String] =
             new File(s"$build_folder/sta/").listFiles
-                .filter(_.toString.endsWith("slack.txt"))
-                .map(file => file.toString.split("/").last -> file)
+                .filter(file => (new File(file + "/slack.txt").exists()))
+                .map(file => file.getName -> (file + "/slack.txt"))
                 .toMap
 
         val wns_map: Map[String, String] = sta_files.map { entry =>
             val file = entry._2
             val name = entry._1
-            val wns  = scala.io.Source.fromFile(file).toString()
+            val wns  = scala.io.Source.fromFile(file).getLines().mkString
             name -> wns
         }.toMap
 
@@ -56,13 +52,44 @@ object Docs {
             case (name, gates) =>
                 name -> (gates, wns_map(name))
         }
-        for ((name, (gates, wns)) <- joined_map) {
-            val content = s"""|Module: $name
-                              |Gates: $gates
-                              |WNS: $wns
-                              |""".stripMargin
-            writeFile(s"$build_folder/docs/$name.txt", content)
-        }
+
+        val texTableEntries = joined_map
+            .map { entry =>
+                val name  = entry._1
+                val gates = entry._2._1
+                val wns   = entry._2._2
+                s"\\texttt{$name} & $gates & $wns \\\\ \\hline"
+            }
+            .mkString("\n")
+
+        val texTable = s"""
+\\renewcommand*{\\arraystretch}{1.25}
+\\begingroup
+\\small
+\\rowcolors{2}{gray!30}{gray!10}
+\\arrayrulecolor{gray!80}
+\\begin{longtable}{|p{0.35\\textwidth}|p{0.20\\textwidth}|p{0.20\\textwidth}|}
+\\hline
+\\rowcolor{gray}
+\\textcolor{white}{\\textbf{Config Name}} & \\textcolor{white}{\\textbf{Gates}} & \\textcolor{white}{\\textbf{WNS}} \\\\ \\hline
+\\endfirsthead
+
+\\hline
+\\rowcolor{gray}
+\\textcolor{white}{\\textbf{Config Name}} & \\textcolor{white}{\\textbf{Gates}} & \\textcolor{white}{\\textbf{WNS}} \\\\ \\hline
+\\endhead
+
+\\hline
+\\endfoot
+$texTableEntries
+\\end{longtable}
+\\captionof{table}{Synth and STA Results}
+\\label{table:synth_sta}
+\\endgroup
+"""
+
+        val texTableClean = texTable.replaceAll("_", "-")
+        writeFile(s"$build_folder/doc/synth_sta.tex", texTableClean)
 
     }
 
@@ -75,6 +102,7 @@ object Docs {
       */
     private def writeFile(path: String, content: String): Unit = {
         val file = new File(path)
+        file.getParentFile.mkdirs()
         file.createNewFile()
         val bw = new java.io.BufferedWriter(new java.io.FileWriter(file))
         bw.write(content)
